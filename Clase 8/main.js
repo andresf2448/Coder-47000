913672bae82fc8f3d525681f7164f9f154f56667
andresf2448 <andresf2448@gmail.com>
2023-08-01 17:23:47 -0500

    Clase 8

---
+++ Clase 8/main.js
@@ -0,0 +1,41 @@
+//MATH
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.max(3, 5, 7, 9, 99, 4, 2, 33, 3, 555));
+// console.log(Math.min(3, 5, 7, 9, 99, 4, 2, 33, 3, 555));
+
+// console.log(Math.max(3, 5, 7, 9, Infinity, 99, 4, 2, 33, 3, 555));
+// console.log(Math.min(3, 5, 7, 9, 99, -Infinity, 4, 2, 33, 3, 555));
+
+// console.log(Math.ceil(3.45)); //aproxima al entero próximo mayor
+// console.log(Math.floor(3.8)) ; //aproxima al entero próximo menor
+// console.log(Math.round(3.8)); //aproxima al entero próximo
+// console.log(Math.round(3.1));
+
+// console.log(Math.sqrt(9));
+// console.log(Math.sqrt(2));
+// console.log(Math.sqrt(1));
+// console.log(Math.sqrt(-16));
+
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log(Math.random() * 10);
+// console.log(Math.random() * 10);
+// console.log(Math.round(Math.random() * 10));
+// console.log(Math.round(Math.random() * 7 + 10));
+// console.log(Math.round(Math.random() * 7 + 10));
+// console.log(Math.round(Math.random() * 7 + 10));
+// console.log(Math.round(Math.random() * 7 + 10));
+
+// const generarAleatorio = (amplitud, desplazamiento) => {
+//   return Math.round(Math.random() * amplitud + desplazamiento);
+// };
+
+// console.log(generarAleatorio(27, 10));
+// console.log(generarAleatorio(27, 10));
+// console.log(generarAleatorio(27, 10));
+// console.log(generarAleatorio(27, 10));
+
+//crear una funcion que genere nombres aleatorios
