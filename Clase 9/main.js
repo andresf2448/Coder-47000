9ccd42e12ad7896f6ec88bf5a0ec237124d634ce
andresf2448 <andresf2448@gmail.com>
2023-08-03 18:21:11 -0500

    Clase 9

--- Clase 9/main.js
+++ Clase 9/main.js
@@ -24,16 +24,47 @@
 // console.log(divs[1].innerHTML);
 // console.log(divs[2].innerHTML);
 
-let saludo = document.getElementById("saludo");
-let seccion = prompt("Ingrese la seccion que desea");
+// let saludo = document.getElementById("saludo");
+// let seccion = prompt("Ingrese la seccion que desea");
 
-if (seccion === "carrito") {
-  saludo.innerHTML = "<h1>Bienvenido al carrito</h1>";
-  saludo.className = "rojo";
-} else if (seccion === "favoritos") {
-  saludo.innerHTML = "<h1>Bienvenido a los favoritos</h1>";
-  saludo.className = "verde";
-} else {
-  saludo.innerHTML = "<h1>Bienvenido a nuestra super página</h1>";
-  saludo.className = "azul";
-}
+// if (seccion === "carrito") {
+//   saludo.innerHTML = "<h1>Bienvenido al carrito</h1>";
+//   saludo.className = "rojo";
+// } else if (seccion === "favoritos") {
+//   saludo.innerHTML = "<h1>Bienvenido a los favoritos</h1>";
+//   saludo.className = "verde";
+// } else {
+//   saludo.innerHTML = "<h1>Bienvenido a nuestra super página</h1>";
+//   saludo.className = "azul";
+// }
+
+// let container = document.getElementById("container");
+
+//Creación de nodos
+// let parrafo = document.createElement("p"); //creacion de la etiqueta
+// parrafo.innerHTML = '<img src="">'; //asignacion del contenido
+// document.body.append(parrafo); //asignacion de un padres
+// container.append(parrafo);
+
+// let container = document.getElementById("container");
+// container.remove();
+
+// let container = document.getElementById("container");
+
+// const productos = [
+//   { id: 1, nombre: "camisa", precio: 1000 },
+//   { id: 2, nombre: "gorra", precio: 770 },
+//   { id: 3, nombre: "zapato", precio: 350 },
+//   { id: 4, nombre: "media", precio: 1750 },
+// ];
+
+// productos.forEach((item) => {
+//   let div = document.createElement("div");
+//   div.innerHTML = `
+//     <h2>Id: ${item.id}</h2>
+//     <p>Nombre: ${item.nombre}</p>
+//     $${item.precio}
+//   `;
+
+//   container.append(div);
+// });
