a19c8963c515e167e544a85c63996d665a3c770a
andresf2448 <andresf2448@gmail.com>
2023-08-22 17:46:10 -0500

    Clase 14

---
+++ Clase 14/main.js
@@ -0,0 +1,89 @@
+// Swal.fire(
+//   'Good job!',
+//   'You clicked the button!',
+//   'success'
+// )
+
+let boton = document.getElementById("alerta");
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Error!",
+//     text: "Do you want to continue?",
+//     icon: "error",
+//     confirmButtonText: "OK",
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Genial!",
+//     text: "Haz clickeado el botón!",
+//     icon: "success",
+//     confirmButtonText: "Cool",
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Error!",
+//     text: "Error inesperado",
+//     icon: "error",
+//     confirmButtonText: " =( ",
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     position: "center",
+//     icon: "success",
+//     title: "Your work has been saved",
+//     showConfirmButton: false,
+//     timer: 5000,
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Sweet!",
+//     text: "Modal with a custom image.",
+//     imageUrl: "https://unsplash.it/400/200",
+//     imageWidth: 400,
+//     imageHeight: 200,
+//     imageAlt: "Custom image",
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Está seguro de eliminar el producto?",
+//     icon: "warning",
+//     showCancelButton: true,
+//     confirmButtonText: "Sí, seguro",
+//     cancelButtonText: "No, no quiero",
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       Swal.fire({
+//         title: "Borrado!",
+//         icon: "success",
+//         text: "El archivo ha sido borrado",
+//       });
+//     }
+//   });
+// });
+
+// boton.addEventListener("click", () => {
+//   Swal.fire({
+//     title: "Buscar producto en el carrito",
+//     input: "text",
+//     showCancelButton: true,
+//     confirmButtonText: "Look up",
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       //codigo necesario a realizar
+//       Swal.fire({
+//         title: `${result.value}`,
+//       });
+//     }
+//   });
+// });
